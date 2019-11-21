0fc5fc6e79933e56a80127c74bf01db88a38d077
mytree <tape-a@hanmail.net>
2019-11-21 19:49:47 +0900

    빠진 파일 추가

--- Build/Test_QT5/Test/Base/CH10/1001_move_event.h
+++ Build/Test_QT5/Test/Base/CH10/1001_move_event.h
@@ -1,8 +1,4 @@
 
-/*
-	윈도우에서 moveEvent() 호출 안 됨
-*/
-
 #pragma once
 
 #include "../../TestBase.h"
@@ -11,17 +7,37 @@ class C1001_MoveEvent : public QWidget {
 	Q_OBJECT
 
 private:
-	QLabel		*label;
-
+	QLabel		*pLB_mouse;
+	
 public:
 	C1001_MoveEvent() {
 		resize(500, 500);
-		label = new QLabel("Hello World", this);
+		pLB_mouse = new QLabel("Mouse & Key Event Test", this);	pLB_mouse->resize(300, 30);
+		//pLB_mouse = new QLabel("Hello World.", this);	pLB_mouse->resize(300, 30);
+
+		setMouseTracking(true);
 	}
 
 private:
-	void moveEvent(C1001_MoveEvent*) {
-		label->setText(QString("X : %1, Y : %2").arg(pos().x()).arg(pos().y()));
+	//!	@brief	마우스 이동 시 이벤트 처리
+	virtual void mouseMoveEvent(QMouseEvent *pEvent) {
+		QPoint mousePos = pEvent->pos();
+		QString strText = QString("Mouse Moved Pos(%1,%2)").arg(mousePos.x()).arg(mousePos.y());
+		pLB_mouse->setText(strText);
+	}
+
+	//!	@brief	마우스 눌렀다가 떼었을 때 이벤트 처리
+	virtual void mouseReleaseEvent(QMouseEvent *pEvent) {
+		QPoint mousePos = pEvent->pos();
+		//Qt::MouseButtons btns = pEvent->buttons();
+		Qt::MouseButton btn = pEvent->button();
+		QString strText = QString("Mouse Released Pos(%1, %2) Type(%3)").arg(mousePos.x()).arg(mousePos.y()).arg(btn);
+		pLB_mouse->setText(strText);
+	}
+
+	virtual void keyReleaseEvent(QKeyEvent *pEvent) {
+		QString strText = QString("Key Released. Code(%1)").arg(pEvent->nativeVirtualKey());
+		pLB_mouse->setText(strText);
 	}
 };
 
